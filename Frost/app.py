23b3c7eadcf057f0833f292c0329c45223609081
szx <810143012@qq.com>
2020-03-04 23:44:07 +0800

    代码提交

--- Frost/app.py
+++ Frost/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print(username, password)
+        return json.jsonify({"result": "success"})
 
 
 if __name__ == '__main__':
